a1d09df266e26c5a245065728e1dea8218854f21
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2024-08-13 04:45:14 -0700

    Seed flight data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,15 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Airport.create(code: "SFO")
-Airport.create(code: "NYC")
-Airport.create(code: "LAX")
-Airport.create(code: "DEN")
-Airport.create(code: "DFW")
+sfo = Airport.create(code: "SFO")
+nyc = Airport.create(code: "NYC")
+lax = Airport.create(code: "LAX")
+den = Airport.create(code: "DEN")
+dfw = Airport.create(code: "DFW")
+
+Flight.create(departure_airport: sfo, arrival_airport: nyc, start_datetime: DateTime.now + 1.day, duration: 360)
+Flight.create(departure_airport: lax, arrival_airport: sfo, start_datetime: DateTime.now + 2.days, duration: 60)
+Flight.create(departure_airport: dfw, arrival_airport: den, start_datetime: DateTime.now + 3.days, duration: 180)
+Flight.create(departure_airport: nyc, arrival_airport: dfw, start_datetime: DateTime.now + 4.days, duration: 240)
+Flight.create(departure_airport: den, arrival_airport: lax, start_datetime: DateTime.now + 5.days, duration: 120)
+Flight.create(departure_airport: lax, arrival_airport: nyc, start_datetime: DateTime.now + 6.days, duration: 300)
